3c3d9fc041c86b8310fec2a63339536b75465009
李譯騏 <patrick14754@gmail.com>
2020-10-30 22:46:45 +0800

    練習2.3建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,8 +3,9 @@
     <title>@yield('title')|MyBlog</title>
 </head>
 <body>
-<div class="container">
-    @yield ('content')
-</div>
+    @include('layouts.partials.navigation')
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
